a171f292af3e79626dde59879c687f6c043278b5
ruifig <ruimvfigueira@gmail.com>
2014-11-13 14:29:59 +0000

    commented out some test code

--- samples/apcpuos/os/tests.c
+++ samples/apcpuos/os/tests.c
@@ -1,14 +1,40 @@
-#include "appslist.h"
-#include "kernel/kerneldebug.h"
+//#include "appslist.h"
+//#include "kernel/kerneldebug.h"
 
-typedef struct Foobar
+/*
+static double hw_clk_currSecs;
+typedef int (*KrnTimedEventFunc)(void* data1, void* data2);
+typedef struct Kernel
 {
-	int a;
-	int b;
-	char ccc;
-	int* ar[10];
-} Foobar;
+	int timedEvents;
+} Kernel;
+Kernel krn;
 
-typedef Foobar OtherFoobar;
-//OtherFoobar* g_foobar;
-OtherFoobar* g_foobar[2];
+typedef struct KrnTimedEvent
+{
+    // Time at which this event needs to execute
+    float time;
+    KrnTimedEventFunc func;
+    void* data1;
+    void* data2;
+} KrnTimedEvent;
+
+int priorityQueue_peek(void*, void** evt);
+*/
+
+/*
+static int krn_tickTimedEvents()
+{
+    // Wake up any sleeping threads that are due to wake up
+    KrnTimedEvent* evt;
+	priorityQueue_peek(&krn.timedEvents, &evt);
+	if (evt->time <= hw_clk_currSecs)
+		return 1;
+ 
+ //while( priorityQueue_peek(&krn.timedEvents, &evt) &&
+//             (evt->time <= hw_clk_currSecs)) {
+        //priorityQueue_popAndDrop(&krn.timedEvents);
+        //evt->func(evt->data1, evt->data2);
+    //}
+}
+*/
