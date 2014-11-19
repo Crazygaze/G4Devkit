48f8b3537506c15aede522bb286c98c22e771f43
brick <brickbtv@gmail.com>
2014-11-20 02:42:41 +0300

    Sample test of disk drive functions.

--- samples/apcpuos/os/appslist.c
+++ samples/apcpuos/os/appslist.c
@@ -2,22 +2,25 @@
 #include "kernel/kerneldebug.h"
 
 
-#define NUM_APPS 7
+#define NUM_APPS 3
 
 int testapp1(int p1);
 int testapp2(int p1);
 int krn_idleTask(int p1);
 int sysstats_main(int p1);
 
+int file_manager(int p1);
+
 static AppInfo apps[NUM_APPS] =
 {
 	{ "idle", krn_idleTask, TRUE, 800, 0, APPFLAG_NOFOCUS, 0},
 	{ "sysstats", sysstats_main, FALSE, 800, 1024+4000*2, APPFLAG_WANTSKEYS|APPFLAG_WANTSCANVAS|APPFLAG_WANTSSTATUSBAR, 0},
-	{ "testapp1", testapp1, FALSE, 800, 2048+4000, APPFLAG_WANTSCANVAS|APPFLAG_WANTSSTATUSBAR, 1},
+	/*{ "testapp1", testapp1, FALSE, 800, 2048+4000, APPFLAG_WANTSCANVAS|APPFLAG_WANTSSTATUSBAR, 1},
 	{ "testapp2", testapp2, FALSE, 800, 1024+4000, APPFLAG_WANTSCANVAS|APPFLAG_WANTSSTATUSBAR, 2},
 	{ "testapp3", testapp2, FALSE, 800, 1024+4000, APPFLAG_WANTSCANVAS|APPFLAG_WANTSSTATUSBAR, 3},
 	{ "testapp4", testapp2, FALSE, 800, 1024+4000, APPFLAG_WANTSCANVAS|APPFLAG_WANTSSTATUSBAR, 4},
-	{ "testapp5", testapp2, FALSE, 800, 1024+4000, APPFLAG_WANTSCANVAS|APPFLAG_WANTSSTATUSBAR, 5}
+	{ "testapp5", testapp2, FALSE, 800, 1024+4000, APPFLAG_WANTSCANVAS|APPFLAG_WANTSSTATUSBAR, 5},*/
+	{ "file_manager", file_manager, FALSE, 800, 1024+4000*2, APPFLAG_WANTSCANVAS|APPFLAG_WANTSSTATUSBAR, 6}
 };
 
 int os_getNumApps(void)
