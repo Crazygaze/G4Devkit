9cbb6bf7f464721e0ca735fb10974d3d5c9592cf
brick <brickbtv@gmail.com>
2014-11-22 02:38:13 +0300

    FAT detecting, cd, unlink functions

--- samples/apcpuos/filesystem/file_system_provider.h
+++ samples/apcpuos/filesystem/file_system_provider.h
@@ -5,10 +5,28 @@
 #include "stdcshared_defs.h"
 #include <stdlib.h>
 
-int make_file_system(int driveNum);
+#include "extern/fatfs/src/ff.h"
+#include "extern/fatfs/src/diskio.h"
+
+bool make_file_system(int driveNum);
 
 bool is_disk_exist(int driveNum);
+bool is_dir_exist(const char * path);
 bool change_drive(int driveNum);
 bool is_file_system_exist();
+
+typedef enum FILE_TYPE{
+	T_FILE = 0, 
+	T_DIR
+} FILE_TYPE;
+
+typedef struct FILE{
+	char name[20];
+	FILE_TYPE type;
+} FILE;
+
+FILE * get_subdirs(const char * path, int * size);
+bool make_dir(const char * dir_name);
+bool unlink(const char * link);
 
 #endif
